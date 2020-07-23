ff16f9923e39e0eb2c7c3918dd5a25a7533d3e22
remiliacn <bilibililvnima@gmail.com>
2020-07-23 14:43:13 -0700

    Fix: The visual issue caused by calculation
    Fix: The visual issue caused by floating point calculation.

--- userscript.js
+++ userscript.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Chegg tutor earning helper.
 // @author       remiliacn
-// @match        https://www.chegg.com/tutors/archive/
+// @match        https://www.chegg.com/tutors/*
 // @grant        none
 // ==/UserScript==
 
@@ -13,12 +13,18 @@
         let priceSum = 0.0;
         // Get price tags
         const priceList = document.querySelectorAll('span._6zAgqawnLfo40Z0WHSMP');
+        if (priceList.length == 0) {
+            return;
+        }
+
         for (var i = 0; i < priceList.length; i++) {
             let moneyString = priceList[i].innerText.trim().substring(1);
             let money = parseFloat(moneyString);
             // If is void or something that is not a price, record it as 0 otherwise record the price.
             priceSum += (isNaN(money)) ? 0.0 : money;
         }
+
+        priceSum = priceSum.toFixed(2);
 
         // Add the total price below "View lessons (live and written) you've completed"
         const parentBox = document.querySelector('div.Q0ecAeFkl5uajoN_SHQpI');
